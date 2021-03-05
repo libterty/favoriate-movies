586e29a59f85337b6c696d284da2f3e2f2a9292a
lib <dreak603@gmail.com>
2021-03-05 22:24:35 +0800

    chore: config prefix endpoint and test api with fte

--- services/src/users/user.service.ts
+++ services/src/users/user.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import HTTPResponse from '../libs/response';
+import * as IShare from '../shares/interfaces';
 
 @Injectable()
 export class UserService {
-  public async getRequest(): Promise<string> {
-    return 'Hello World!';
+  private readonly httpResponse = new HTTPResponse();
+
+  public async getRequest(): Promise<IShare.IResponseBase<string>> {
+    return this.httpResponse.StatusOK("Hello World!");
   }
 }
